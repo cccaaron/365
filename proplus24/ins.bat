91c6f67e471a0a0c4735577bba093bf3e91d69a6
cccaaron <aaron@ccc.co.il>
2026-06-03 11:15:04 +0300

    Update ins.bat

--- proplus24/ins.bat
+++ proplus24/ins.bat
@@ -10,7 +10,7 @@ md %tmp_fold%
 cd %tmp_fold%
 curl -kO https://raw.githubusercontent.com/cccaaron/365/refs/heads/main/proplus24/setup.exe
 curl -kO https://raw.githubusercontent.com/cccaaron/365/refs/heads/main/proplus24/Configuration.xml
-setup.exe /configure Configuration.xml
 cd ..
+%tmp_fold%\setup.exe /configure %tmp_fold%\Configuration.xml
 rd/q /s %tmp_fold%
 del/q "%~f0"&exit
