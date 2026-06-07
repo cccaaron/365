34089b096c50270405b15a959100f3de4b163877
cccaaron <aaron@ccc.co.il>
2026-06-07 08:37:16 +0300

    Add files via upload

---
+++ proplus24/ins.bat
@@ -0,0 +1,16 @@
+@echo off
+net session >nul 2>&1
+if "%~1%errorlevel%" neq "10" (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~f0", "1", "", "runas", 0 > "%tmp%\b.vbs"
+echo CreateObject^("Scripting.FileSystemObject"^).DeleteFile WScript.ScriptFullName >> "%tmp%\b.vbs"
+start wscript.exe "%tmp%\b.vbs"&exit)
+cd/d "%userprofile%\Downloads"
+set tmp_fold=office-%random%
+md %tmp_fold%
+cd %tmp_fold%
+curl -kO https://raw.githubusercontent.com/cccaaron/365/refs/heads/main/proplus24/setup.exe
+curl -kO https://raw.githubusercontent.com/cccaaron/365/refs/heads/main/proplus24/Configuration.xml
+cd ..
+%tmp_fold%\setup.exe /configure %tmp_fold%\Configuration.xml
+rd/q /s %tmp_fold%
+del/q "%~f0"&exit
