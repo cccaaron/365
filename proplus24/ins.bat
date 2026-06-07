bb6ad682342d43faa751cffee054211ef5d01f8e
cccaaron <aaron@ccc.co.il>
2026-06-07 08:47:57 +0300

    Update ins.bat

--- proplus24/ins.bat
+++ proplus24/ins.bat
@@ -6,8 +6,8 @@ cd/d "%userprofile%\Downloads"
 set tmp_fold=office-%random%
 md %tmp_fold%
 cd %tmp_fold%
-curl -kO https://raw.githubusercontent.com/cccaaron/365/refs/heads/main/proplus24/setup.exe
-curl -kO https://raw.githubusercontent.com/cccaaron/365/refs/heads/main/proplus24/Configuration.xml
+curl -LkO https://cccaaron.github.io/365/proplus24/setup.exe
+curl -LkO https://cccaaron.github.io/365/proplus24/Configuration.xml
 cd ..
 %tmp_fold%\setup.exe /configure %tmp_fold%\Configuration.xml
 rd/q /s %tmp_fold%
